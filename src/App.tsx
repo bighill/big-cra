de1fa59e64e458ec141d0d61e263a3260876cd6b
alexander bighill <alex@bighill.org>
2022-03-21 21:10:42 -0700

    Weather example.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { HomePage, SubPage } from "./pages";
-import { Nav, Text } from "./components";
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <Text h="1">Empty app</Text>
-
       <BrowserRouter>
-        <Nav />
-        <hr />
-
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="subpage/*" element={<SubPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,51 @@
-import { CovidAjax, Text } from "../components";
+import { useState } from "react";
+
+const apiKey = "foo";
+
+interface IWeatherWeather {
+  icon: string;
+}
+interface IWeather {
+  name: string;
+  main: {
+    temp: number;
+  };
+  weather: IWeatherWeather[];
+}
 
 export const HomePage = () => {
+  const [weather, setWeather] = useState<IWeather>({
+    name: "",
+    main: { temp: 0 },
+    weather: [{ icon: "" }],
+  });
+
+  const handleGetWeather = async () => {
+    navigator.geolocation.getCurrentPosition(async function (position) {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.lat}&lon=${pos.lng}&appid=${apiKey}`;
+      console.log(weatherUrl);
+
+      const res = await fetch(weatherUrl)
+        .then((r) => r.json())
+        .then((r) => {
+          console.log(r);
+          setWeather(r);
+        });
+    });
+  };
+
   return (
     <>
-      <Text>Style reset</Text>
-      <Text>Sample components</Text>
-      <Text>Simple Jest tests</Text>
-      <Text>React Router v6</Text>
-      <CovidAjax />
+      <div> start here</div>
+      <button onClick={handleGetWeather}>Get</button>
+      <div>Icon: {weather.weather[0].icon}</div>
+      <div>City: {weather.name}</div>
+      <div>Temp: {weather.main.temp - 273}</div>
     </>
   );
 };
